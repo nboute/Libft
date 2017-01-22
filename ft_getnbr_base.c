bd8d71ffddd22656d6bd381ae4027bfe03534189
Nicolas BOUTE <nboute@e1r12p2.42.fr>
2017-01-22 07:30:43 +0100

    repaired getnbrbase

--- ft_getnbr_base.c
+++ ft_getnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: nboute <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 17:40:56 by nboute            #+#    #+#             */
-/*   Updated: 2017/01/20 16:42:51 by nboute           ###   ########.fr       */
+/*   Updated: 2017/01/22 07:30:18 by nboute           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ int	ft_getnbr_base(const char *s, size_t b)
 	int		val;
 	size_t	d;
 
-	if (!s || b < 2 || b > 16)
+	if ((!s || b < 2 || b > 16) && !(i = 0) && !(nb = 0))
 		return (0);
 	d = ft_numdigits_base(s, b);
-	i = 0;
+	while (ft_isalpha(s[i]) && ft_isdigit(s[i]) && s[i] != '-' && s[i] != '+'
+			&& s[i])
+					i++;
 	sign = (s[i] == '-') ? -1 : 1;
 	if (s[i] == '-' || s[i] == '+')
 		i++;
-	nb = 0;
 	while (((s[i] >= '0' && s[i] <= '9') || (b > 10 && ft_tolower(s[i]) >= 'a'
 					&& ft_tolower(s[i] < 'a' + (int)b - 10))) && d-- && s[i])
 	{
 		val = (s[i] >= '0' && s[i] <= '9') ? s[i] - '0' :
 			s[i] - 'a' + 10;
-		if (val >= (int)b && (i))
+		if (val >= (int)b && (i++))
 			return (0);
 		nb += ft_power(b, d) * val;
-		i++;
 	}
 	return (nb * sign);
 }
